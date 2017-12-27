a425d25b69748cf96de2b6bb2698c3988e6b925f
Chris Jagielski <chrisjagielski@gmail.com>
2017-12-27 09:29:54 -0600

    parse command line arg for size of maze

--- generate_maze.py
+++ generate_maze.py
@@ -1,2 +1,23 @@
 # python code to create a maze
 
+# imports
+import argparse
+
+def createMaze(n=6):
+    print "Now creating a maze of size", n
+    
+
+if __name__ == "__main__":
+    print "creating a maze"
+
+    parser = argparse.ArgumentParser(description='Optional app description')
+    parser.add_argument('-n', type=int, help='The size of the maze, should be an even number.')
+    args = parser.parse_args()
+
+    print 'args:', args
+
+    n = 6
+    if args.n:
+        n = args.n
+
+    createMaze(n)
