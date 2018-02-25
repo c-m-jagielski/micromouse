47101ddde05ee091bbe4f7432595ac4ca5c4e874
Chris Jagielski <chrisjagielski@gmail.com>
2018-02-25 10:08:09 -0500

    break out maze mapping and path solving sections

--- SimulateMouse.py
+++ SimulateMouse.py
@@ -5,6 +5,29 @@
 import argparse
 from GenerateMaze import GenerateMaze
 
+class SimulateMouse(object):
+    """
+    Simulate the mouse... go through the maze and map it out (discover walls) then solve the best path
+    """
+
+    def __init__(self):
+        print "inside simulation..."
+        # TODO: do I need anything here?
+
+    def generateMazeMap(self, maze):
+        """
+        maze
+          An array (n*n x 4 size) that tells the existence of walls for each position in the maze
+        """
+        print "now the 'mouse' is mapping out the maze..."
+        mazeKnowledge = []
+        return mazeKnowledge
+
+    def generateBestPath(self, mazeKnowledge):
+        print "now generating the best path..."
+        bestPath = []
+        return bestPath
+
 if __name__ == "__main__":
     print "simulating the maze!"
 
@@ -23,5 +46,11 @@ if __name__ == "__main__":
         n = 6
 
     # first generate the maze
+    print "now generating the maze..."
     generateMaze = GenerateMaze()
-    generateMaze.createMaze(n)
+    maze = generateMaze.createMaze(n)
+
+    # now run the simulation
+    simulateMouse = SimulateMouse()
+    mazeKnowledge = simulateMouse.generateMazeMap(maze)
+    bestPath = simulateMouse.generateBestPath(mazeKnowledge)
